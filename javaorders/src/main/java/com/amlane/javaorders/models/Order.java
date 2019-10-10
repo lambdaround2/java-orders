a7dea7c5caeb9d58c2877d31f0d3391af6613046
amlane <twelvexstring@gmail.com>
2019-10-09 23:33:50 -0500

    basic table schema completed/1st foreign key relationship established

---
+++ javaorders/src/main/java/com/amlane/javaorders/models/Order.java
@@ -0,0 +1,80 @@
+package com.amlane.javaorders.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ordnum;
+
+    private double ordamount;
+    private double advanceamount;
+    private String orderdescription;
+
+    private long custcode;
+
+    public Order(double ordamount, double advanceamount, String orderdescription, long custcode)
+    {
+        this.ordamount = ordamount;
+        this.advanceamount = advanceamount;
+        this.orderdescription = orderdescription;
+        this.custcode = custcode;
+    }
+
+    public Order()
+    {
+    }
+
+    public long getOrdnum()
+    {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum)
+    {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount()
+    {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount)
+    {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount()
+    {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount)
+    {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrderdescription()
+    {
+        return orderdescription;
+    }
+
+    public void setOrderdescription(String orderdescription)
+    {
+        this.orderdescription = orderdescription;
+    }
+
+    public long getCustcode()
+    {
+        return custcode;
+    }
+
+    public void setCustcode(long custcode)
+    {
+        this.custcode = custcode;
+    }
+}
